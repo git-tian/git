f1106d4a850799abc9e24aedac8376c83a51bb7f
git-tian <git-tian131425>
2020-07-26 19:24:17 +0800

    提交

---
+++ git_demo/src/main/java/com/tian/Demo.java
@@ -0,0 +1,12 @@
+package com.tian;
+
+/**
+ * @author qingtian
+ * @Package com.tian
+ * @date 2020/7/26 19:16
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("测试git push");
+    }
+}
